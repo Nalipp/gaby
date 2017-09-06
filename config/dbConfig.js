fc958b2fa88ea7a1ee0aaf17df89cfe42cd53476
Nalipp <nateplipp@gmail.com>
2017-09-06 12:37:10 +0900

    update landing

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,8 +3,7 @@ var mongoose = require('mongoose');
 if (process.env.NODE_ENV === 'dev') {
   url = 'mongodb://localhost:27017/gaby';
 } else {
-  // url = 'mongodb://heroku_m7jkxr6b:3371vuqjk1vvb7tg7usjd715j0@ds151993.mlab.com:51993/heroku_m7jkxr6b';
-  url = 'mongodb://localhost:27017/gabyProduction';
+  url = 'mongodb://heroku_m7jkxr6b:3371vuqjk1vvb7tg7usjd715j0@ds151993.mlab.com:51993/heroku_m7jkxr6b';
 }
 
 mongoose.connect(url, err => {
